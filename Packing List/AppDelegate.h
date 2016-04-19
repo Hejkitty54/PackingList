d71be71baa37994f343a9d9be1ef3aefd07136a6
Hejkitty54 <kozue.kishimoto@.gmail.com>
2016-04-19 13:23:19 +0200

    Initial Commit

---
+++ Packing List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Packing List
+//
+//  Created by ITHS on 2016-04-19.
+//  Copyright © 2016 Kozue Wendén. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Packing List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Packing List
+//
+//  Created by ITHS on 2016-04-19.
+//  Copyright © 2016 Kozue Wendén. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
